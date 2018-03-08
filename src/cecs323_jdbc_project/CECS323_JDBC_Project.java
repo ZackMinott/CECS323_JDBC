5e2a498cd480ca7bee0f3b23cccdbee6f754aa6b
Richie Espinosa <Richie@richiegfx.net>
2018-03-08 12:13:48 -0800

    Initial Commit

---
+++ src/cecs323_jdbc_project/CECS323_JDBC_Project.java
@@ -0,0 +1,12 @@
+package cecs323_jdbc_project;
+
+import java.sql.*;
+
+
+public class CECS323_JDBC_Project {
+
+    public static void main(String[] args) {
+        // TODO Set up JDB Driver Connection & Settings
+    }
+    
+}
