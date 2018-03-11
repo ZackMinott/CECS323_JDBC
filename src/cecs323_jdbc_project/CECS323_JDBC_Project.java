497db64863817cd6da18ce90b352e34df85bcc67
Richie Espinosa <Richie@richiegfx.net>
2018-03-11 11:30:58 -0700

    Menu w/ main goals. Still need specific search for Groups, publishers, and books

--- src/cecs323_jdbc_project/CECS323_JDBC_Project.java
+++ src/cecs323_jdbc_project/CECS323_JDBC_Project.java
@@ -1,12 +1,34 @@
 package cecs323_jdbc_project;
 
 import java.sql.*;
+import java.util.Scanner;
 
 
 public class CECS323_JDBC_Project {
 
     public static void main(String[] args) {
+        
+        menu();
+        
         // TODO Set up JDB Driver Connection & Settings
+    }
+    
+    public static void menu() {
+        
+        // Variables
+        int menuSelection;
+        
+        // Menu
+        System.out.println("\t-----\t Menu \t -----");
+        System.out.println("[1]\tList Groups");
+        System.out.println("[2]\tList Publishers");
+        System.out.println("[3]\tList Books");
+        System.out.println("------------------------");
+        System.out.println("[4]\tAdd New Book");
+        System.out.println("[5]\tAdd & Replace Publisher");
+        System.out.println("------------------------");
+        System.out.println("[0]\tDelete Book");
+        System.out.println("\n\nSelection: ");
     }
     
 }
